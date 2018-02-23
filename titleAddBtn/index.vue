f8f8007c60cf87fddbbc603b4e2c11fdbf52a84b
tiangao-office <tiangao@edenep.net>
2018-02-23 16:32:48 +0800

    v-for 动态函数

---
+++ titleAddBtn/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div v-for="item in addBtnList" class="add-btn" @click="clkCall(item.methods)">
+      <i class="add-btn-i" :class="item.icon"></i>
+      <div>{{item.title}}</div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'titleAddBtn',
+    props: ['addBtnList'],
+    methods: {
+      clkCall(methodsWords) {
+        this.$emit('clkCallBk', methodsWords)
+      }
+    }
+  }
+</script>
